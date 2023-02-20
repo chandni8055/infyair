e4034c4fc88b8b486cf72c7dc31c565c902192e9
chandni8055 <95699966+chandni8055@users.noreply.github.com>
2023-02-20 15:27:53 +0530

    initial commit

---
+++ src/main/java/com/infosys/infyair/InfyairApplication.java
@@ -0,0 +1,13 @@
+package com.infosys.infyair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfyairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfyairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infosys/infyair/InfyairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.infyair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfyairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
